74edf9bb1f57d6689e8521776da423bc130c9b03
Tambari Fyneface <fynetammy@gmail.com>
2023-09-20 07:03:15 -0700

    a c programmed that prints a line into the sandard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a c programme that prints a line onto the standard error
+ * Return: 1 (the programmed should return 1, which in this case is success
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
